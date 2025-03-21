4397346164425fc059c4685dd43a79c1d0a1ddd6
sona.rohal <srohalova@gmail.com>
2025-03-21 09:49:05 +0100

    funkcia selectPlan

---
+++ index.js
@@ -0,0 +1,5 @@
+const selectPlan = (planNumber) => {
+  document.querySelector("#plan1").classList.remove("plan--selected");
+  document.querySelector("#plan2").classList.remove("plan--selected");
+  document.querySelector("#plan3").classList.remove("plan--selected");
+} 
