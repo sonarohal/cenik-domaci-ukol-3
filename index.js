b49b15e597da2a407c576922774d0e6e9b830063
sona.rohal <srohalova@gmail.com>
2025-03-21 10:19:48 +0100

    test - plan selected

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const selectPlan = (planNumber) => {
   document.querySelector("#plan1").classList.remove("plan--selected");
   document.querySelector("#plan2").classList.remove("plan--selected");
   document.querySelector("#plan3").classList.remove("plan--selected");
-} 
+
+  const selectedPlanElem = document.querySelector(`#plan${planNumber}`);
+  selectedPlanElem.classList.add("plan--selected");
+};
+
+selectPlan(3); 
